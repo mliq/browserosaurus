c3e24b6eceb561183ffab742cfe0f87556b5f1de
Michael Liquori <mliquori@nerdery.com>
2017-10-10 10:21:35 -0500

    Add my custom browser menu options

--- src/browsers.js
+++ src/browsers.js
@@ -1,14 +1,14 @@
 export default [
-  { name: 'Google Chrome Canary', alias: 'Canary', key: 'a' },
-  { name: 'Brave', key: 'b' },
-  { name: 'Chromium', key: 'c' },
-  { name: 'Firefox', key: 'f' },
-  { name: 'FirefoxNightly', alias: 'Nightly', key: 'n' },
-  { name: 'Google Chrome', key: 'g' },
-  { name: 'Maxthon', key: 'm' },
-  { name: 'Min', key: 'i' },
-  { name: 'Opera', key: 'o' },
-  { name: 'Safari', key: 's' },
-  { name: 'TorBrowser', key: 't' },
-  { name: 'Vivaldi', key: 'v' }
+  { name: 'Google Chrome Canary', alias: 'd = Home', key: 'd' },
+    //  { name: 'Brave', key: 'b' },
+    // { name: 'Chromium', key: 'c' },
+    //{ name: 'Firefox', key: 'f' },
+    // { name: 'FirefoxNightly', alias: 'Nightly', key: 'n' },
+  { name: 'Google Chrome', alias: 'a = Work', key: 'a' },
+    //{ name: 'Maxthon', key: 'm' },
+    //{ name: 'Min', key: 'i' },
+    //{ name: 'Opera', key: 'o' },
+  { name: 'Safari', alias: 'z = Safari', key: 'z' },
+    //{ name: 'TorBrowser', key: 't' },
+    //{ name: 'Vivaldi', key: 'v' }
 ]
